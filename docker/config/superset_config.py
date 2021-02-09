9da89d06f539b2f4f43da88fa9308ff3f1e585ea
Jordan Guinn <jordan@skylight.digital>
2021-02-08 18:49:54 -0800

    oh, lol

--- docker/config/superset_config.py
+++ docker/config/superset_config.py
@@ -1,3 +1,4 @@
 import os
 APP_ICON = '/static/assets/images/mounted_images/logo.png'
+APP_ICON_WIDTH = 35
 SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://superset_admin:{os.environ["POSTGRES_PASSWORD"]}@superset_db/superset'
