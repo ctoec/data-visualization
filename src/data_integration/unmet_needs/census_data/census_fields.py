30cac02228a4288d14ba9e4b2f61869c10745e06
Kyle Magida <kmagida110@users.noreply.github.com>
2021-03-01 14:35:46 -0500

    census scripts (#25)
    * census scripts
    
    * Update .gitignore
    
    * Update pull_census_data.py
    
    * add wrapper to pull census data
    
    * Update pull_census_data.py
    
    * more docs

---
+++ src/data_integration/unmet_needs/census_data/census_fields.py
@@ -0,0 +1,5 @@
+CENSUS_FIELDS = {'B01003_001E': 'total_population',
+                 'B19113_001E': 'median_income',
+                 'B01001_003E': 'pop_male_under_5',
+                 'B01001_027E': 'pop_female_under_5'
+                 }
