3222a2c344c9c986c8a17ebf4780e25565f64d3d
Kyle Magida <kmagida110@users.noreply.github.com>
2021-03-17 10:55:49 -0400

    zfill county and sub county (#36)
    * zfill county and sub county
    
    * Enable Javascript

--- src/demand_estimation/calculate_town_demand.py
+++ src/demand_estimation/calculate_town_demand.py
@@ -6,12 +6,14 @@ SMI_ELIGIBLE_CHILDREN_FILE = "town_estimates.csv"
 OUT_FILE = "final_demand_by_town.csv"
 
 towns = pd.read_csv(BASE_TOWN_FILE)
-towns.drop(columns=[c for c in towns.columns if not c in ['NAME', 'COUSUBFP']], inplace=True)
+towns.drop(columns=[c for c in towns.columns if not c in ['NAME', 'COUSUBFP','COUNTYFP']], inplace=True)
 towns.rename(columns={'NAME': 'town'}, inplace=True)
 parent_metric_df = pd.read_csv(CALCULATED_PARENT_METRIC_FILE)
 smi_df = pd.read_csv(SMI_ELIGIBLE_CHILDREN_FILE)
 df = towns.merge(parent_metric_df, on='town')
 df = df.merge(smi_df, on='town')
 df.drop(columns=['min_full_estimate', 'max_full_estimate'], inplace=True)
-df.rename(columns={'COUSUBFP': 'county_code', 'demand_estimate_working_parents': 'estimated_demand_by_care_availability', 'mid_full_estimate': 'children_eligible_for_care_services'}, inplace=True)
+df.rename(columns={'COUNTYFP': 'county_code','COUSUBFP': 'sub_county_code', 'demand_estimate_working_parents': 'estimated_demand_by_care_availability', 'mid_full_estimate': 'children_eligible_for_care_services'}, inplace=True)
+df['county_code'] = df['county_code'].astype(str).str.zfill(3)
+df['sub_county_code'] = df['sub_county_code'].astype(str).str.zfill(5)
 df.to_csv(OUT_FILE, index=False)
