c6f9e54682be406c27b440e8f46cf0dfcea113df
Kyle Magida <kyle@omelas.co>
2021-03-30 14:09:17 -0400

    add caching

--- docker/config/superset_config.py
+++ docker/config/superset_config.py
@@ -4,3 +4,6 @@ APP_ICON_WIDTH = 35
 SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://superset_admin:{os.environ["POSTGRES_PASSWORD"]}@superset_db/superset'
 MAPBOX_API_KEY=f'{os.environ["MAPBOX_KEY"]}'
 ENABLE_JAVASCRIPT_CONTROLS = True
+
+CACHE_DIR='/home/ec2-user/docker'
+CACHE_TYPE='FileSystemCache'
