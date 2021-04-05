5419f6c543a625dc3dd97f73e00affd498121b0a
Kyle Magida <kyle@omelas.co>
2021-04-05 11:04:09 -0400

    add automated caching

--- docker/config/superset_config.py
+++ docker/config/superset_config.py
@@ -1,4 +1,5 @@
 import os
+from celery.schedules import crontab
 APP_ICON = '/static/assets/images/mounted_images/logo.png'
 APP_ICON_WIDTH = 35
 SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://superset_admin:{os.environ["POSTGRES_PASSWORD"]}@superset_db/superset'
@@ -12,3 +13,28 @@ CACHE_CONFIG = {"CACHE_TYPE": "filesystem",
 # Cache for datasource metadata and query results
 DATA_CACHE_CONFIG = {"CACHE_TYPE": "filesystem",
                      "CACHE_DIR": "/cache"}
+
+CELERYBEAT_SCHEDULE = {
+        "email_reports.schedule_hourly": {
+            "task": "email_reports.schedule_hourly",
+            "schedule": crontab(minute=1, hour="*"),
+        },
+        "reports.scheduler": {
+            "task": "reports.scheduler",
+            "schedule": crontab(minute="*", hour="*"),
+        },
+        "reports.prune_log": {
+            "task": "reports.prune_log",
+            "schedule": crontab(minute=0, hour=0),
+        },
+        # From https://superset.apache.org/docs/installation/cache
+        'cache-warmup-daily': {
+                'task': 'cache-warmup',
+                'schedule': crontab(minute=0, hour=6),  # Refresh daily, cache will last for 24 hours on site
+                'kwargs': {
+                    'strategy_name': 'top_n_dashboards',
+                    'top_n': 10,
+                    'since': '1 month ago',
+                    }
+        }
+    }
