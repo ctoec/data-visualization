4c06c1e610c18ae87ce2a9750d1c0c05b02f6f2d
Kyle Magida <kyle@omelas.co>
2021-04-08 14:41:47 -0400

    name capacity

--- src/data_integration/ece_data/space_pull.sql
+++ src/data_integration/ece_data/space_pull.sql
@@ -3,8 +3,8 @@ SELECT id as funding_space_id,
        ageGroup as funding_space_age_group,
        time as funding_space_time,
        organizationId as organization_id,
-       CASE capacity WHEN -1 THEN null else capacity END
+       capacity
 FROM funding_space
 -- Only CDC and Smart Start currently have valid capacity numbers. Once we have a system for collecting
 -- School Readiness data those funding streams can be included
-WHERE source IN ('CDC', 'SS')
+WHERE source IN ('CDC', 'SS') and capacity != -1
