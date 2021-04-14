11beaf9f9fa57191a60fb2839112b789095d7111
Jordan Guinn <jordan@skylight.digital>
2021-04-13 18:45:05 -0700

    and this

--- .platform/hooks/postdeploy/create-admin-user.sh
+++ .platform/hooks/postdeploy/create-admin-user.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [ ! -f .initialized ]; then                                                                                                                                                                                    
     echo "Creating admin user..."  
 
