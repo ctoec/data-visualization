fdf29d51bfbceff55c028d4f1c44f8b8d7d3b9a2
Jordan Guinn <jordan@skylight.digital>
2021-04-13 18:30:17 -0700

    ahhhhh here we go

--- docker/config/superset_config.py
+++ docker/config/superset_config.py
@@ -2,7 +2,7 @@ import os
 from celery.schedules import crontab
 APP_ICON = '/static/assets/images/mounted_images/logo.png'
 APP_ICON_WIDTH = 35
-SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://pensieveadmin:{os.environ["POSTGRES_PASSWORD"]}@{os.environ["STAGE"]}.ece-pensieve-db.ctoecskylight.com/superset'
+SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{os.environ["POSTGRES_USER"]}:{os.environ["POSTGRES_PASSWORD"]}@{os.environ["POSTGRES_HOST"]}/{os.environ["POSTGRES_DB"]}'
 MAPBOX_API_KEY=f'{os.environ["MAPBOX_KEY"]}'
 ENABLE_JAVASCRIPT_CONTROLS = True
 
