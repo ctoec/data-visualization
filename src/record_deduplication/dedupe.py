9dc1578e0230bffae6bfbb02a0483f071edc9fe2
bamader <brandon@skylight.digital>
2021-04-16 14:00:50 -0400

    Add record deduplication

---
+++ src/record_deduplication/dedupe.py
@@ -0,0 +1,122 @@
+from recordlinkage import Index, Compare
+import pandas as pd
+from builtins import isinstance
+
+'''
+To reference more information about deduplication with recordlinkage,
+consult the docs and a working example at:
+https://recordlinkage.readthedocs.io/en/latest/notebooks/data_deduplication.html
+'''
+
+CHILD_FILE = 'ct_child_data.csv'
+OUT_FILE = 'deduped_ece_child_ids.csv'
+
+
+def identify_duplicates(df, show_ranks_distribution=True):
+    '''
+    Identifies duplicates in a given data frame that consists of
+    child records with a variety of fields. For the identification
+    procedure to succeed, the df MUST contain the following fields,
+    named as such:
+      - id
+      - birthdate
+      - sasid
+      - uniqueId
+      - firstName
+      - middleName
+      - lastName
+    Additionally, the df will be typecast to strings before finding
+    duplicates, so it's advisable to pass in just a dataframe with
+    these given columns. 
+    '''
+    print('-------Casting Data Frame-------')
+    # Empty strings are 'compared as' equal under exact blocking
+    # This lets us check for sasids when they're there but not
+    # care if they aren't
+    df.fillna('', inplace=True)
+    df.loc[:, df.columns != 'id'] = df.drop('id', axis=1).apply(lambda x: x.astype(str).str.title())
+
+    print('-------Indexing Candidate Matches-------')
+    indexer = Index()
+    # Blocking on sasids gives too many candidates because na's and
+    # '' are treated as matching pairs--this gives 92 million candidates...
+    # Also, many children are missing sasids, but there's only a single
+    # record that doesn't have a birthdate, making it the best initial
+    # candidate selector
+    indexer.sortedneighbourhood('birthdate', window=3)
+    candidates = indexer.index(df)
+    print(str(len(candidates)) + ' candidate pairs identified')
+
+    print('-------Building similarity scores-------')
+    comparator = Compare()
+    # Forcing an exact match here is fine because of the empty string
+    # stuff above
+    comparator.exact('sasid', 'sasid', label='sasid')
+    comparator.exact('uniqueId', 'uniqueId', label='uniqueId')
+
+    # These are threshold scores based on probability of being the
+    # same string, just misspelled
+    # Thresholds are based on a combination of guidance from the example
+    # and some experimentation around quality of matches
+    comparator.string('firstName', 'firstName', threshold=0.85, label='firstName')
+    comparator.string('middleName', 'middleName', threshold=0.85, label='middleName')
+    comparator.string('lastName', 'lastName', threshold=0.85, label='lastName')
+    comparator.string('birthdate', 'birthdate', threshold=0.85, label='birthdate')
+    features = comparator.compute(candidates, df)
+
+    if show_ranks_distribution:
+        # With six features and two exact matches, all candidates will score
+        # between 2 and 6; each name field and birthdate that matches is +1.
+        # We just need to decide the cutoff level. 6's are guaranteed matches.
+        # In 200+ 5's, I saw MAYBE one false positive, so those are good too.
+        # 4's get tricky and look 50/50 on whether one of the names are 
+        # significantly different.
+        print("")
+        print('Distribution of ranks:')
+        print(features.sum(axis=1).value_counts().sort_index(ascending=False))
+        print("")
+        
+    print('-------Thresholding Matches-------')
+    # Decided on keeping only records that have at least 5 score points.
+    # These are the highest quality matches and don't prune out any records
+    # that aren't true duplicates.
+    matches = features[features.sum(axis=1) >= 5]
+    print(str(len(matches)) + ' duplicates identified')
+    print("")
+                 
+    # Start by building a dictionary pointing duplicates to the original
+    dupes = {}
+    for idx in matches.index:
+        dupes[df.iloc[idx[1]]['id']] = df.iloc[idx[0]]['id']
+    
+    # Now assign each child their own unique ID for DB joining purposes
+    ece_ids = list(df['id'].values)
+    new_ids = {}
+    i = 1
+    # Start with a first pass to make sure all "original" records are given
+    # an id, since duplicates are in no particular referential order
+    for id in ece_ids:
+        if not id in dupes:
+            new_ids[id] = i
+            i += 1
+        else:
+            new_ids[id] = dupes[id]
+    # Now replace everything that points to another record with the
+    # reference record's ID number
+    for key in new_ids:
+        if isinstance(new_ids[key], str):
+            new_ids[key] = new_ids[new_ids[key]]
+    new_ids = [new_ids[id] for id in ece_ids]
+    
+    # Write the output to a new CSV
+    out = pd.DataFrame()
+    out['ece_ids'] = ece_ids
+    out['new_ids'] = new_ids
+    out.to_csv(OUT_FILE, index=False)
+        
+
+if __name__ == '__main__':
+    df = pd.read_csv(CHILD_FILE)
+    identify_duplicates(df, show_ranks_distribution=False)
+
+
