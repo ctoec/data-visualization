c1d14f42b0ce9355790d97cf41a5b12e49892f84
Jordan Guinn <jordan@skylight.digital>
2021-04-21 11:03:31 -0700

    smh

--- .platform/hooks/postdeploy/99_init_superset.sh
+++ .platform/hooks/postdeploy/99_init_superset.sh
@@ -21,18 +21,13 @@ if [ ! -f .superset-init ]; then
     docker exec superset superset db upgrade
 
     echo "Setting default Superset permissions..."
-    docker exec -it superset superset init
+    docker exec superset superset init
 
     echo "Creating Superset admin user..."  
     STAGE=$(/opt/elasticbeanstalk/bin/get-config environment -k BUILD_ENV)
     PASSWORD=$(aws secretsmanager get-secret-value --secret-id /pensieve/$STAGE/superset/admin/password --query 'SecretString' --output text --region 'us-east-2')
 
-    docker exec -it superset superset fab create-admin \
-      --username 'voldemort' \
-      --firstname 'Admin' \
-      --lastname 'User' \
-      --email 'admin@ctoec.org' \
-      --password $PASSWORD
+    docker exec superset superset fab create-admin --username 'voldemort' --firstname 'Admin' --lastname 'User' --email 'admin@ctoec.org' --password $PASSWORD
 
     touch .superset-init     
     echo "Superset initialization complete!"                                                                                                                                                                     
