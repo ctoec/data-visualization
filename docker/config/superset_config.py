933e22cc79ebd9aee8f4f9fc189114ea0440cfb0
Jordan Guinn <jordan@skylight.digital>
2021-04-26 14:56:41 -0700

    SIGH

--- docker/config/superset_config.py
+++ docker/config/superset_config.py
@@ -3,7 +3,7 @@ from celery.schedules import crontab
 APP_ICON = '/static/assets/images/mounted_images/logo.png'
 APP_ICON_WIDTH = 35
 SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{os.environ["POSTGRES_USER"]}:{os.environ["POSTGRES_PASSWORD"]}@{os.environ["POSTGRES_HOST"]}/{os.environ["POSTGRES_DB"]}'
-MAPBOX_API_KEY=f'{os.environ["MAPBOX_KEY"]}'
+MAPBOX_API_KEY=f'{os.environ["MAPBOX_API_KEY"]}'
 ENABLE_JAVASCRIPT_CONTROLS = True
 ENABLE_PROXY_FIX = True
 
